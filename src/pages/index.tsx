158e3a06a773d2a75b54f44b8a8c504d1687f84e
Yeony99 <ny.yeony.kim@gmail.com>
2023-08-23 16:40:54 +0900

    feat: update dashboard publishing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { io } from "socket.io-client"
 
 function HomePage() {
   const [receivedData, setReceivedData] = useState('');
 
-  // useEffect(() => {
-  //   const socket = io("/api/subscriber", { transports: ["websocket"] });
-  //   socket.on("connect", () => {
-  //     console.log("소켓 생성!");
-  //   });
-
-  //   socket.on("getData", (data) => {
-  //     console.log(data);
-  //     setReceivedData(data);
-  //   });
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log("changed?!?", receivedData);
-  // }, [receivedData]);
-
-  // useEffect(() => {
-  //   const socket = io('http://localhost:3001', { transports: ['websocket'] })
-  //   socket.on('connect', () => {
-  //     console.log('소켓 생성!')
-  //   })
+  useEffect(() => {
+    const socket = io('http://localhost:3001', { transports: ['websocket'] })
+    socket.on('connect', () => {
+      console.log('소켓 생성!')
+    })
   
-  //   socket.on('getData', data => {
-  //     console.log(data)
-  //     setReceivedData(data);
-  //   })
-  // }, [])
-  // useEffect(() => {
-
-  //   console.log('changed?!?', receivedData)
-  // }, [receivedData]);
-
+    socket.on('getData', data => {
+      console.log(data)
+      setReceivedData(data);
+    })
+  }, [])
+  
   return (
     <div>
+      <h1>테스트 페이지</h1>
       <h1>Received Data: {receivedData}</h1>
     </div>
   );
--- src/pages/potenit.tsx
+++ src/pages/potenit.tsx
@@ -1,15 +1,127 @@
 import ChartComponent from "@/components/DonutChart";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+interface RobotData {
+  RemainingTaskCount: number;
+  OngoingTaskCount: number;
+  CompletedTaskCount: number;
+  NewTaskCount: number;
+  CumulativeTaskSpeed: number;
+  AverageTaskSpeed: number;
+  TaskProgress: [robotId: string, progress: string];
+  RobotTaskStatus: [robotId: string, taskId: string, taskName: string, status: "start" | "progress" | "pause" | "complete"];
+  BatteryRemain: [robotId: string, batteryStatus: number];
+  BatteryStatus: [robotId: string, batteryStatus: "Using" | "Charging"];
+  PerfirmableTaskCount: [robotId: string, count: number];
+}
+
+interface RobotStatusData {
+  [key: string]: any;
+}
+
+interface ObjectProps {
+  [key: string]: any;
+}
+
+function getKeyValueObject(str: string) {
+  let match = str.match(/\((\w+) (.+?)\)/);
+
+  if (match) {
+    let [, key, values] = match;
+    let parsedValues: (string | number)[] | number;
+
+    if (/"([^"]+)"/.test(values)) {
+      parsedValues =
+        values.split(' ').map((match) => isNaN(parseInt(match)) ? match.slice(1, -1) : parseInt(match)) || [];
+        console.log(values)
+    } else {
+      parsedValues = parseInt(values);
+    }
+
+    console.log('parsedValues:', parsedValues)
+
+    let output: { [key: string]: (string | number)[] | number } = {
+      [key]: parsedValues,
+    };
+    console.log('output: ', output)
+    return output;
+  } else {
+    console.log("No valid input found.");
+    return {};
+  }
+}
+
+function getRobotDataByRobotId(arr: (string|number)[]) {
+
+}
 
 export default function Potenit() {
-    
-    const chartData = [
-        { name: 'Red', value: 12 },
-        { name: 'Blue', value: 19 },
-        { name: 'Yellow', value: 3 },
-        { name: 'Green', value: 5 },
-        { name: 'Purple', value: 2 },
-        { name: 'Orange', value: 3 },
-      ];
+  const chartData = [
+    { name: "Red", value: 12 },
+    { name: "Blue", value: 19 },
+  ];
+
+  const [receivedData, setReceivedData] = useState<RobotData>({
+    RemainingTaskCount: 0,
+    OngoingTaskCount: 0,
+    CompletedTaskCount: 0,
+    NewTaskCount: 0,
+    CumulativeTaskSpeed: 0,
+    AverageTaskSpeed: 0,
+    TaskProgress: ["", ""],
+    RobotTaskStatus: ["", "", "", "progress"],
+    BatteryRemain: ["", 0],
+    BatteryStatus: ["", "Using"],
+    PerfirmableTaskCount: ["", 0],
+  });
+
+  const [robotStatus, setRobotStatus] = useState<RobotStatusData>({
+    AMR_LIFT1: {},
+    AMR_LIFT2: {},
+    AMR_LIFT3: {},
+    AMR_LIFT4: {},
+    PALLETIZER1: {}
+  });
+
+  useEffect(() => {
+    const socket = io("http://localhost:3001", { transports: ["websocket"] });
+    socket.on("connect", () => {
+      console.log("소켓 생성!");
+    });
+
+    socket.on("getData", (data) => {
+      setReceivedData((prevReceivedData) => {
+        const tempData = getKeyValueObject(data);
+        return { ...prevReceivedData, ...tempData };
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    setRobotStatus((prevRobotStatus) => {
+      const temp:ObjectProps = {};
+      if(receivedData.PerfirmableTaskCount[0]) {
+        temp[`${receivedData.PerfirmableTaskCount[0]}`] = {PerfirmableTaskCount: receivedData.PerfirmableTaskCount[1]}
+      }
+      return { ...prevRobotStatus, ...temp};
+    })
+  }, [receivedData.PerfirmableTaskCount])
+
+  useEffect(() => {
+    setRobotStatus((prevRobotStatus) => {
+      const temp:ObjectProps = {};
+      if(receivedData.TaskProgress[0]) {
+        temp[`${receivedData.TaskProgress[0]}`] = {TaskProgress: receivedData.TaskProgress[1]}
+      }
+      return { ...prevRobotStatus, ...temp};
+    })
+  }, [receivedData.TaskProgress])
+
+  // @TODO 나머지 value들도 ...
+
+  console.log('robotStatus', robotStatus)
+
   return (
     <>
       <div className="flex text-center">
@@ -24,7 +136,7 @@ export default function Potenit() {
               </h5>
               <div className="flex items-baseline text-gray-900 dark:text-white justify-center">
                 <span className="text-5xl font-extrabold tracking-tight">
-                  49
+                  {receivedData.RemainingTaskCount}
                 </span>
                 <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
                   건
@@ -37,7 +149,7 @@ export default function Potenit() {
               </h5>
               <div className="flex items-baseline text-gray-900 dark:text-white justify-center">
                 <span className="text-5xl font-extrabold tracking-tight">
-                  49
+                  {receivedData.OngoingTaskCount}
                 </span>
                 <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
                   건
@@ -50,7 +162,7 @@ export default function Potenit() {
               </h5>
               <div className="flex items-baseline text-gray-900 dark:text-white justify-center">
                 <span className="text-5xl font-extrabold tracking-tight">
-                  49
+                  {receivedData.CompletedTaskCount}
                 </span>
                 <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
                   건
@@ -65,7 +177,7 @@ export default function Potenit() {
               </h5>
               <div className="flex items-baseline text-gray-900 dark:text-white justify-center">
                 <span className="text-5xl font-extrabold tracking-tight">
-                  49
+                  {receivedData.NewTaskCount}
                 </span>
                 <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
                   건
@@ -78,7 +190,7 @@ export default function Potenit() {
               </h5>
               <div className="flex items-baseline text-gray-900 dark:text-white justify-center">
                 <span className="text-5xl font-extrabold tracking-tight">
-                  49
+                  {receivedData.CumulativeTaskSpeed}
                 </span>
                 <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
                   건
@@ -87,11 +199,11 @@ export default function Potenit() {
             </div>
             <div className="basis-1/3 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-4 dark:bg-gray-800 dark:border-gray-700">
               <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
-                로봇 현황표
+                평균 작업 속도
               </h5>
               <div className="flex items-baseline text-gray-900 dark:text-white justify-center">
                 <span className="text-5xl font-extrabold tracking-tight">
-                  49
+                  {receivedData.AverageTaskSpeed}
                 </span>
                 <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
                   건
@@ -103,16 +215,64 @@ export default function Potenit() {
             <div className="mt-8 bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3 mb-3">
               <p className="font-bold">로봇 현황표</p>
             </div>
-            <div className="flex">
-            <ChartComponent data={chartData} />
-            <ChartComponent data={chartData} />
-            <ChartComponent data={chartData} />
-            <ChartComponent data={chartData} />
-            <ChartComponent data={chartData} />
+            <div className="flex justify-around">
+              <div className="w-full">
+                <div className="font-bold">AMR_Lift1</div>
+                <div>진행도 {robotStatus['AMR_LIFT1'] ? robotStatus['AMR_LIFT1'].TaskProgress : "-/-"}</div>
+                <div className="mt-3 font-bold">현재 진행중인 작업</div>
+                <div> status</div>
+                <div className="mt-4">배터리 상황</div>
+                <ChartComponent data={chartData} />
+                <div className="mt-4 font-bold">
+                  배터리 잔량에 따른 수행 가능 작업 수
+                </div>
+                <div>{robotStatus['AMR_LIFT1'].PerfirmableTaskCount ? robotStatus['AMR_LIFT1'].PerfirmableTaskCount : 0}건</div>
+              </div>
+              <div className="w-full">
+                <div className="font-bold">AMR_Lift2</div>
+                <div>진행도</div>
+                <div className="mt-3 font-bold">현재 진행중인 작업</div>
+                <div>taskName status</div>
+                <div className="mt-4">배터리 상황</div>
+                <ChartComponent data={chartData} />
+                <div className="mt-4 font-bold">
+                  배터리 잔량에 따른 수행 가능 작업 수
+                </div>
+                <div>10건</div>
+              </div>
+              <div className="w-full">
+                <div className="font-bold">AMR_Lift3</div>
+                <div>진행도</div>
+                <div className="mt-3 font-bold">현재 진행중인 작업</div>
+                <div>taskName status</div>
+                <div className="mt-4">배터리 상황</div>
+                <ChartComponent data={chartData} />
+                <div className="mt-4 font-bold">
+                  배터리 잔량에 따른 수행 가능 작업 수
+                </div>
+                <div>10건</div>
+              </div>
+              <div className="w-full">
+                <div className="font-bold">AMR_Lift4</div>
+                <div>진행도</div>
+                <div className="mt-3 font-bold">현재 진행중인 작업</div>
+                <div>taskName status</div>
+                <div className="mt-4">배터리 상황</div>
+                <ChartComponent data={chartData} />
+                <div className="mt-4 font-bold">
+                  배터리 잔량에 따른 수행 가능 작업 수
+                </div>
+                <div>10건</div>
+              </div>
+              <div className="w-full">
+                <div className="font-bold">PALLETIZER1</div>
+                <div>진행도</div>
+                <div className="mt-3 font-bold">현재 진행중인 작업</div>
+                <div>taskName status</div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="w-full">ff</div>
       </div>
     </>
   );
