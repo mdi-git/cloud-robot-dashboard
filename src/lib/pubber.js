f3145e67f17b1dc80bf89bf052ef7615340274a8
Yeony99 <ny.yeony.kim@gmail.com>
2023-08-23 16:40:13 +0900

    feat: zeromq pub-sub demo

--- src/lib/pubber.js
+++ src/lib/pubber.js
@@ -9,5 +9,5 @@ let i = 0;
 
 setInterval(function() {
   console.log("1씩 늘리기...", i++);
-  sock.send([`test ${i}`]);
-}, 2000);
+  sock.send(i%2 === 0 ? `(RemainingTaskCount ${i})` : `(PerfirmableTaskCount "AMR_LIFT1" ${i})`);
+}, 5000);
--- src/lib/subber.js
+++ src/lib/subber.js
@@ -1,37 +1,32 @@
 const express = require('express');
 const app = express();
-const http = require('http')
+const http = require('http');
 const server = http.createServer(app);
-const {Server} = require('socket.io');
+const { Server } = require('socket.io');
 const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000/',
-        methods: ['GET', 'POST'],
-        credentials:true
-    }
+  cors: {
+    origin: 'http://localhost:3000', // Remove trailing slash
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
 });
 const port = 3001;
-var zmq = require("zeromq"),
-  sock = zmq.socket("sub");
-  
+const zmq = require('zeromq');
+const sock = zmq.socket('sub');
 
-sock.connect("tcp://127.0.0.1:51316");
-sock.subscribe("");
-console.log("Subscriber connected to port 51316");
+sock.connect('tcp://127.0.0.1:51316');
+sock.subscribe('');
+console.log('Subscriber connected to port 51316');
 
+sock.on('message', function (topic, message) {
+  console.log('메시지 받음:', topic.toString('utf-8'));
+  io.emit('getData', topic.toString('utf-8'));
+});
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    sock.on("message", function(topic, message) {
-        console.log(
-          "메시지 받음 :",
-          topic.toString('utf-8')
-        );
-      
-        io.emit('getData', topic.toString('utf-8'))
-      });
-})
+  console.log('a user connected');
+});
 
 server.listen(port, () => {
-    console.log(`서버 실행 at http://localhost:${port}`)
-})
+  console.log(`서버 실행 at http://localhost:${port}`);
+});
