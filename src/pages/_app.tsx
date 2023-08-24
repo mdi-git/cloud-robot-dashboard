b5188217970bc2d136305d3656edfd0c4a8ec2b0
Yeony99 <ny.yeony.kim@gmail.com>
2023-08-24 12:14:39 +0900

    feat: update _app.tsx to persist store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,23 @@
+import store from "@/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+export let persistor = persistStore(store);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <div className="p-12">
-        <Component {...pageProps} />
-      </div>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+        <div className="p-12">
+          <Component {...pageProps} />
+        </div>
+        </PersistGate>
+      
+      </Provider>
     </>
   );
 }
