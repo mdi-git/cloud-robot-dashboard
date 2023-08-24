899bd33f6173da66d1d048ec16a2f8ec019d66d5
Yeony99 <ny.yeony.kim@gmail.com>
2023-08-24 12:14:12 +0900

    feat: update Chart component

--- src/components/DonutChart.tsx
+++ src/components/DonutChart.tsx
@@ -1,28 +1,36 @@
-import { ResponsiveContainer, PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import {
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+} from "recharts";
 import { useState, useEffect } from "react";
 
 interface DonutChartProps {
   data: any[]; // The data format will depend on your chart data
 }
 
-const COLORS = [
-  "#0088FE",
-  "#00C49F",
-];
+const COLORS = ["#fe1c00", "#0088FE"];
 
 const DonutChart: React.FC<DonutChartProps> = ({ data }) => {
   const [isClient, setIsClient] = useState(false);
-
+  const [propData, setPropData] = useState(data);
   useEffect(() => {
     setIsClient(true);
   }, []);
+
+  useEffect(() => {
+    setPropData(data);
+  }, [data]);
   return (
     <>
       {isClient && (
         <ResponsiveContainer width="100%" height={230} minWidth={200}>
           <PieChart>
             <Pie
-              data={data}
+              data={propData}
               dataKey="value"
               nameKey="name"
               innerRadius="50%"
