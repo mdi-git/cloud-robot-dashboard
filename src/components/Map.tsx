53827a86837423ff0266f7c64926c5d77fbddd69
Yeony99 <ny.yeony.kim@gmail.com>
2023-09-04 15:09:36 +0900

    feat: real map coordinate publishing

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -11,7 +11,7 @@ interface Point {
 }
 
 interface ObjectProps {
-  [key: string]: any
+  [key: string]: any;
 }
 interface CoordinatePlaneProps {
   data: Point[];
@@ -21,14 +21,17 @@ interface CoordinatePlaneProps {
 const CoordinatePlane: React.FC<CoordinatePlaneProps> = ({ data, obj }) => {
   const maxCoordinate = 70; // Maximum x and y coordinate values
   const dotRadius = 8;
-  console.log(obj['147'] )
-  
-  const [newDotPosition, setNewDotPosition] = useState({ x:( obj['147'][0] / maxCoordinate) *  2450, y: 830 - ((obj['147'][1] / maxCoordinate) * 4850) });
+  console.log(obj["147"]);
+
+  const [newDotPosition1, setNewDotPosition1] = useState({
+    x: (obj["147"][0] / maxCoordinate) * 2450,
+    y: 1000 - (obj["147"][1] / maxCoordinate) * 3800 - 170,
+  });
 
   // useEffect(() => {
 
   //   const timer = setInterval(() => {
-  //     setNewDotPosition((prev) => {
+  //     setNewDotPosition1((prev) => {
   //       return {x: prev.x++, y: prev.y++}
   //     })
   //   }, 1000)
@@ -42,62 +45,247 @@ const CoordinatePlane: React.FC<CoordinatePlaneProps> = ({ data, obj }) => {
     const stage = new Konva.Stage({
       container: "coordinate-plane-container",
       width: window.innerWidth - 30,
-      height: 830,
+      height: 1000,
     });
 
     const layer = new Konva.Layer();
 
-    // Draw x-axis
-    const xAxis = new Konva.Line({
-      points: [30, stage.height()-15, stage.width() - 145, stage.height()-15],
-      stroke: "black",
-    });
-
-    // Draw y-axis
-    const yAxis = new Konva.Line({
-      points: [30, 0, stage.height() - 800, stage.height() - 13],
-      stroke: "black",
-    });
-
-    for(let i = 0; i <= 6; i += 1) {
-      const label = new Konva.Label({
-        x: 5,
-        y: (i * 2) + (i * 132),
-        draggable: true,
+    // 우측 1번째
+    {
+      const xAxis1 = new Konva.Line({
+        // x1, y1, x2, y2, x3, y3
+        points: [650, 50, stage.width() - 385, 50],
+        stroke: "red",
       });
-  
-      label.add(
-        new Konva.Text({
-          text: (12-(i*2)).toString(),
-          fontSize: 15,
-          lineHeight: 1.2,
-          fill: "black",
-        })
-      );
-      layer.add(label);
+
+      const xAxis2 = new Konva.Line({
+        points: [650, 160, stage.width() - 385, 160],
+        stroke: "orange",
+      });
+
+      const xAxis3 = new Konva.Line({
+        points: [650, 95, stage.width() - 385, 95],
+        stroke: "red",
+      });
+
+      const xAxis4 = new Konva.Line({
+        points: [650, 115, stage.width() - 385, 115],
+        stroke: "red",
+      });
+
+      const yAxis1 = new Konva.Line({
+        points: [650, 50, 650, 160],
+        stroke: "blue",
+      });
+
+      const yAxis2 = new Konva.Line({
+        points: [stage.width() - 385, 50, stage.width() - 385, 160],
+        stroke: "blue",
+      });
+
+      layer.add(xAxis1);
+      layer.add(xAxis2);
+      layer.add(xAxis3);
+      layer.add(xAxis4);
+      layer.add(yAxis1);
+      layer.add(yAxis2);
+      for (let i = 0; i < 8; i++) {
+        layer.add(
+          new Konva.Line({
+            points: [705 + 100 * i, 50, 705 + 100 * i, 160],
+            stroke: "blue",
+          })
+        );
+      }
     }
 
-    for(let i = 0; i <= 10; i += 1) {
-      const label = new Konva.Label({
-        x: 30+  (i * 170),
-        y: 817,
-        draggable: true,
+    // 우측 2번째
+    {
+      const xAxis1 = new Konva.Line({
+        // x1, y1, x2, y2, x3, y3
+        points: [650, 365, stage.width() - 385, 365],
+        stroke: "orange",
       });
-  
-      label.add(
-        new Konva.Text({
-          text: ((i*5)).toString(),
-          fontSize: 15,
-          lineHeight: 1.2,
-          fill: "black",
-        })
-      );
-      layer.add(label);
+
+      const xAxis2 = new Konva.Line({
+        points: [650, 410, stage.width() - 385, 410],
+        stroke: "orange",
+      });
+
+      const xAxis3 = new Konva.Line({
+        points: [650, 430, stage.width() - 385, 430],
+        stroke: "orange",
+      });
+
+      const xAxis4 = new Konva.Line({
+        points: [650, 480, stage.width() - 385, 480],
+        stroke: "red",
+      });
+
+      const yAxis1 = new Konva.Line({
+        points: [650, 365, 650, 480],
+        stroke: "blue",
+      });
+
+      const yAxis2 = new Konva.Line({
+        points: [stage.width() - 385, 365, stage.width() - 385, 480],
+        stroke: "blue",
+      });
+
+      layer.add(xAxis1);
+      layer.add(xAxis2);
+      layer.add(xAxis3);
+      layer.add(xAxis4);
+      layer.add(yAxis1);
+      layer.add(yAxis2);
+      for (let i = 0; i < 8; i++) {
+        layer.add(
+          new Konva.Line({
+            points: [700 + 100 * i, 365, 700 + 100 * i, 480],
+            stroke: "blue",
+          })
+        );
+      }
     }
+
+    // 우측 3번째
+
+    {
+      const xAxis1 = new Konva.Line({
+        // x1, y1, x2, y2, x3, y3
+        points: [650, 680, stage.width() - 385, 680],
+        stroke: "orange",
+      });
+
+      const xAxis2 = new Konva.Line({
+        points: [650, 720, stage.width() - 385, 720],
+        stroke: "red",
+      });
+
+      const yAxis1 = new Konva.Line({
+        points: [650, 680, 650, 720],
+        stroke: "blue",
+      });
+
+      const yAxis2 = new Konva.Line({
+        points: [stage.width() - 385, 680, stage.width() - 385, 720],
+        stroke: "blue",
+      });
+
+      layer.add(xAxis1);
+      layer.add(xAxis2);
+      layer.add(yAxis1);
+      layer.add(yAxis2);
+      for (let i = 0; i < 8; i++) {
+        layer.add(
+          new Konva.Line({
+            points: [700 + 100 * i, 680, 700 + 100 * i, 720],
+            stroke: "blue",
+          })
+        );
+      }
+    }
+
+    // 좌측 1번째
+    {
+      const xAxis1 = new Konva.Line({
+        // x1, y1, x2, y2, x3, y3
+        points: [220, 170, 320, 170],
+        stroke: "orange",
+      });
+
+      const xAxis2 = new Konva.Line({
+        points: [220, 410, 320, 410],
+        stroke: "orange",
+      });
+
+      const yAxis1 = new Konva.Line({
+        points: [220, 170, 220, 410],
+        stroke: "orange",
+      });
+
+      const yAxis2 = new Konva.Line({
+        points: [320, 170, 320, 410],
+        stroke: "orange",
+      });
+
+      layer.add(xAxis1);
+      layer.add(xAxis2);
+      layer.add(yAxis1);
+      layer.add(yAxis2);
+    }
+
+    // 좌측 2번째
+    {
+      const xAxis1 = new Konva.Line({
+        // x1, y1, x2, y2, x3, y3
+        points: [210, 535, 350, 535],
+        stroke: "orange",
+      });
+
+      const xAxis2 = new Konva.Line({
+        points: [210, 690, 350, 690],
+        stroke: "orange",
+      });
+
+      const yAxis1 = new Konva.Line({
+        points: [210, 535, 210, 690],
+        stroke: "orange",
+      });
+
+      const yAxis2 = new Konva.Line({
+        points: [350, 535, 350, 690],
+        stroke: "orange",
+      });
+
+      layer.add(xAxis1);
+      layer.add(xAxis2);
+      layer.add(yAxis1);
+      layer.add(yAxis2);
+    }
+    // // Draw y-axis
+    // const yAxis = new Konva.Line({
+    //   points: [30, 0, stage.height() - 800, stage.height() - 13],
+    //   stroke: "black",
+    // });
+
+    // for(let i = 0; i <= 6; i += 1) {
+    //   const label = new Konva.Label({
+    //     x: 5,
+    //     y: (i * 2) + (i * 132),
+    //     draggable: true,
+    //   });
+
+    //   label.add(
+    //     new Konva.Text({
+    //       text: (12-(i*2)).toString(),
+    //       fontSize: 15,
+    //       lineHeight: 1.2,
+    //       fill: "black",
+    //     })
+    //   );
+    //   layer.add(label);
+    // }
+
+    // for(let i = 0; i <= 10; i += 1) {
+    //   const label = new Konva.Label({
+    //     x: 30+  (i * 170),
+    //     y: 817,
+    //     draggable: true,
+    //   });
+
+    //   label.add(
+    //     new Konva.Text({
+    //       text: ((i*5)).toString(),
+    //       fontSize: 15,
+    //       lineHeight: 1.2,
+    //       fill: "black",
+    //     })
+    //   );
+    //   layer.add(label);
+    // }
     // Add x-axis and y-axis to the layer
-    layer.add(xAxis);
-    layer.add(yAxis);
-
+    // layer.add(yAxis);
 
     // Draw points and edges
     data.forEach((point) => {
@@ -107,14 +295,14 @@ const CoordinatePlane: React.FC<CoordinatePlaneProps> = ({ data, obj }) => {
       // const scaledX = (x / maxCoordinate) * stage.width();
       const scaledX = (x / maxCoordinate) * 2450;
       // const scaledY = (y / maxCoordinate) * stage.height();
-      const scaledY = (y / maxCoordinate) * 4850;
+      const scaledY = (y / maxCoordinate) * 3800 + 170;
 
       // Draw a circle for the point
       const circle = new Konva.Circle({
         x: scaledX,
         y: stage.height() - scaledY,
         radius: dotRadius,
-        fill: "lightblue",
+        fill: point.name === 500 || point.name === 501 ? "purple" : (point.name >= 1 && point.name <= 58 ? "orange" : "greenyellow")
       });
 
       const label = new Konva.Label({
@@ -144,7 +332,7 @@ const CoordinatePlane: React.FC<CoordinatePlaneProps> = ({ data, obj }) => {
           // const scaledEdgeX = (edgeX / maxCoordinate) * stage.width();
           const scaledEdgeX = (edgeX / maxCoordinate) * 2450;
           // const scaledEdgeY = (edgeY / maxCoordinate) * stage.height();
-          const scaledEdgeY = (edgeY / maxCoordinate) * 4850;
+          const scaledEdgeY = (edgeY / maxCoordinate) * 3800 + 170;
 
           const line = new Konva.Line({
             points: [
@@ -163,54 +351,54 @@ const CoordinatePlane: React.FC<CoordinatePlaneProps> = ({ data, obj }) => {
     });
 
     // Draw grid lines along the x-axis
-    for (let i = 1; i <= maxCoordinate; i+=5.3) {
-      const scaledX = (i / maxCoordinate) * 2260; // Adjust the scaling factor as needed
+    // for (let i = 1; i <= maxCoordinate; i+=5.3) {
+    //   const scaledX = (i / maxCoordinate) * 2260; // Adjust the scaling factor as needed
 
-      const verticalLine = new Konva.Line({
-        points: [scaledX, 10, scaledX, stage.height()],
-        stroke: "lightgray",
-        dash: [10, 10], // Dashed pattern for grid lines
-      });
+    //   const verticalLine = new Konva.Line({
+    //     points: [scaledX, 10, scaledX, stage.height()],
+    //     stroke: "lightgray",
+    //     dash: [10, 10], // Dashed pattern for grid lines
+    //   });
 
-      layer.add(verticalLine);
-    }
+    //   layer.add(verticalLine);
+    // }
 
-    // Draw grid lines along the y-axis
-    for (let i = -1.7; i <= maxCoordinate; i+=4.3) {
-      const scaledY = (i / maxCoordinate) * 2200; // Adjust the scaling factor as needed
+    // // Draw grid lines along the y-axis
+    // for (let i = -1.7; i <= maxCoordinate; i+=4.3) {
+    //   const scaledY = (i / maxCoordinate) * 2200; // Adjust the scaling factor as needed
 
-      const horizontalLine = new Konva.Line({
-        points: [
-          50,
-          stage.height() - scaledY - 70,
-          stage.width() - 150,
-          stage.height() - scaledY - 70,
-        ],
-        stroke: "lightgray",
-        dash: [10, 10], // Dashed pattern for grid lines
-      });
+    //   const horizontalLine = new Konva.Line({
+    //     points: [
+    //       50,
+    //       stage.height() - scaledY - 70,
+    //       stage.width() - 150,
+    //       stage.height() - scaledY - 70,
+    //     ],
+    //     stroke: "lightgray",
+    //     dash: [10, 10], // Dashed pattern for grid lines
+    //   });
 
-      layer.add(horizontalLine);
-    }
+    //   layer.add(horizontalLine);
+    // }
 
     const newDot = new Konva.Circle({
-      x: newDotPosition.x,
-      y: newDotPosition.y,
+      x: newDotPosition1.x,
+      y: newDotPosition1.y,
       radius: dotRadius,
       fill: "green", // Choose a color for the new dot
       draggable: true,
-      onDragEnd: (event: { target: any; }) => {
+      onDragEnd: (event: { target: any }) => {
         const dot = event.target;
         const dotX = dot.x();
         const dotY = dot.y();
-        setNewDotPosition({ x: dotX, y: dotY });
+        setNewDotPosition1({ x: dotX, y: dotY });
       },
     });
 
     layer.add(newDot);
     // Add the layer to the stage
     stage.add(layer);
-  }, [data, newDotPosition]);
+  }, [data, newDotPosition1]);
 
   return (
     <div id="coordinate-plane-container">
--- src/pages/map/real.tsx
+++ src/pages/map/real.tsx
@@ -2,13 +2,17 @@
 import React from 'react';
 import CoordinatePlane from '@/components/Map';
 import {Coordinates, ObjNamePos} from "../../components/mapPos"
+import { useAppSelector } from '@/hooks';
 
 const Home: React.FC = () => {
+  
+  const selector = useAppSelector((state) => state.data)
+  const robotData = {...JSON.parse(selector.potenitChart)}
   return (
     <div>
-      <CoordinatePlane data={Coordinates} obj={ObjNamePos}/>
+      <CoordinatePlane data={Coordinates} obj={ObjNamePos} />
     </div>
   );
-};
+}; 
 
 export default Home;
